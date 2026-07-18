ae12f10258efbd11e22907a4683156f90fdbda88
Leon-CYL <leonlin0106@gmail.com>
2026-07-17 21:11:47 -0700

    Update part a code for performance

--- part_a/tasksys.cpp
+++ part_a/tasksys.cpp
@@ -4,7 +4,6 @@
 #include <condition_variable>
 #include <algorithm>
 
-
 IRunnable::~IRunnable() {}
 
 ITaskSystem::ITaskSystem(int num_threads) {}
@@ -16,28 +15,34 @@ ITaskSystem::~ITaskSystem() {}
  * ================================================================
  */
 
-const char* TaskSystemSerial::name() {
+const char *TaskSystemSerial::name()
+{
     return "Serial";
 }
 
-TaskSystemSerial::TaskSystemSerial(int num_threads): ITaskSystem(num_threads) {
+TaskSystemSerial::TaskSystemSerial(int num_threads) : ITaskSystem(num_threads)
+{
 }
 
 TaskSystemSerial::~TaskSystemSerial() {}
 
-void TaskSystemSerial::run(IRunnable* runnable, int num_total_tasks) {
-    for (int i = 0; i < num_total_tasks; i++) {
+void TaskSystemSerial::run(IRunnable *runnable, int num_total_tasks)
+{
+    for (int i = 0; i < num_total_tasks; i++)
+    {
         runnable->runTask(i, num_total_tasks);
     }
 }
 
-TaskID TaskSystemSerial::runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
-                                          const std::vector<TaskID>& deps) {
+TaskID TaskSystemSerial::runAsyncWithDeps(IRunnable *runnable, int num_total_tasks,
+                                          const std::vector<TaskID> &deps)
+{
     // You do not need to implement this method.
     return 0;
 }
 
-void TaskSystemSerial::sync() {
+void TaskSystemSerial::sync()
+{
     // You do not need to implement this method.
     return;
 }
@@ -48,47 +53,58 @@ void TaskSystemSerial::sync() {
  * ================================================================
  */
 
-const char* TaskSystemParallelSpawn::name() {
+const char *TaskSystemParallelSpawn::name()
+{
     return "Parallel + Always Spawn";
 }
 
-TaskSystemParallelSpawn::TaskSystemParallelSpawn(int num_threads): ITaskSystem(num_threads) {
+TaskSystemParallelSpawn::TaskSystemParallelSpawn(int num_threads) : ITaskSystem(num_threads)
+{
     this->num_threads_ = num_threads;
 }
 
-TaskSystemParallelSpawn::~TaskSystemParallelSpawn() {
+TaskSystemParallelSpawn::~TaskSystemParallelSpawn()
+{
 }
 
-void TaskSystemParallelSpawn::run(IRunnable* runnable, int num_total_tasks) {
+void TaskSystemParallelSpawn::run(IRunnable *runnable, int num_total_tasks)
+{
     int num_workers = std::min(this->num_threads_, num_total_tasks);
     this->next_task_id_.store(0, std::memory_order_relaxed);
     std::vector<std::thread> threads;
-    for (int i = 0; i < num_workers; i++) {
+    for (int i = 0; i < num_workers; i++)
+    {
         threads.emplace_back(&TaskSystemParallelSpawn::Worker, this, num_total_tasks, runnable);
     }
 
-    for (int i = 0; i < num_workers; i++) {
+    for (int i = 0; i < num_workers; i++)
+    {
         threads[i].join();
     }
 }
 
-void TaskSystemParallelSpawn::Worker(int num_task, IRunnable* runnable) {
-    while (this->next_task_id_.load(std::memory_order_acquire) < num_task) {
+void TaskSystemParallelSpawn::Worker(int num_task, IRunnable *runnable)
+{
+    while (this->next_task_id_.load(std::memory_order_acquire) < num_task)
+    {
         int id = this->next_task_id_.fetch_add(1, std::memory_order_relaxed);
-        if (id >= num_task) {
+        if (id >= num_task)
+        {
             break;
         }
         runnable->runTask(id, num_task);
     }
 }
 
-TaskID TaskSystemParallelSpawn::runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
-                                                 const std::vector<TaskID>& deps) {
+TaskID TaskSystemParallelSpawn::runAsyncWithDeps(IRunnable *runnable, int num_total_tasks,
+                                                 const std::vector<TaskID> &deps)
+{
     // You do not need to implement this method.
     return 0;
 }
 
-void TaskSystemParallelSpawn::sync() {
+void TaskSystemParallelSpawn::sync()
+{
     // You do not need to implement this method.
     return;
 }
@@ -99,31 +115,37 @@ void TaskSystemParallelSpawn::sync() {
  * ================================================================
  */
 
-const char* TaskSystemParallelThreadPoolSpinning::name() {
+const char *TaskSystemParallelThreadPoolSpinning::name()
+{
     return "Parallel + Thread Pool + Spin";
 }
 
-TaskSystemParallelThreadPoolSpinning::TaskSystemParallelThreadPoolSpinning(int num_threads): ITaskSystem(num_threads) {
+TaskSystemParallelThreadPoolSpinning::TaskSystemParallelThreadPoolSpinning(int num_threads) : ITaskSystem(num_threads)
+{
     this->shut_down_ = false;
     this->has_task_ = false;
     this->active_claimers_ = 0;
-    int active_threads = (num_threads > 8) ? 8 : num_threads;
+    int active_threads = (num_threads > 16) ? 16 : num_threads;
     this->num_threads_ = (active_threads > 1) ? active_threads - 1 : 1;
-    for (int i = 0; i < this->num_threads_; i++) {
+    for (int i = 0; i < this->num_threads_; i++)
+    {
         thread_pool.emplace_back(&TaskSystemParallelThreadPoolSpinning::workerLoop, this);
     }
 }
 
-TaskSystemParallelThreadPoolSpinning::~TaskSystemParallelThreadPoolSpinning() {
+TaskSystemParallelThreadPoolSpinning::~TaskSystemParallelThreadPoolSpinning()
+{
     this->has_task_ = false;
     this->shut_down_ = true;
 
-    for (int i = 0; i < this->num_threads_; i++) {
+    for (int i = 0; i < this->num_threads_; i++)
+    {
         thread_pool[i].join();
     }
 }
 
-void TaskSystemParallelThreadPoolSpinning::run(IRunnable* runnable, int num_total_tasks) {
+void TaskSystemParallelThreadPoolSpinning::run(IRunnable *runnable, int num_total_tasks)
+{
     mutex_.lock();
     this->next_task_id_.store(0, std::memory_order_relaxed);
     this->completed_tasks_.store(0, std::memory_order_relaxed);
@@ -132,13 +154,18 @@ void TaskSystemParallelThreadPoolSpinning::run(IRunnable* runnable, int num_tota
     this->has_task_.store(true, std::memory_order_release);
     mutex_.unlock();
 
-    while (this->completed_tasks_.load(std::memory_order_acquire) < num_total_tasks) {
+    while (this->completed_tasks_.load(std::memory_order_acquire) < num_total_tasks)
+    {
         int id = this->next_task_id_.fetch_add(1, std::memory_order_relaxed);
-        if (id < num_total_tasks) {
+        if (id < num_total_tasks)
+        {
             runnable->runTask(id, num_total_tasks);
             this->completed_tasks_.fetch_add(1, std::memory_order_release);
-        } else {
-            while (this->completed_tasks_.load(std::memory_order_acquire) < num_total_tasks) {
+        }
+        else
+        {
+            while (this->completed_tasks_.load(std::memory_order_acquire) < num_total_tasks)
+            {
                 // Spin
             }
             break;
@@ -146,28 +173,35 @@ void TaskSystemParallelThreadPoolSpinning::run(IRunnable* runnable, int num_tota
     }
 
     this->has_task_.store(false, std::memory_order_release);
-    while (this->active_claimers_.load(std::memory_order_acquire) > 0) {
+    while (this->active_claimers_.load(std::memory_order_acquire) > 0)
+    {
         // Spin
     }
 }
 
-void TaskSystemParallelThreadPoolSpinning::workerLoop() {
+void TaskSystemParallelThreadPoolSpinning::workerLoop()
+{
     bool shut_down = this->shut_down_.load();
 
-    while(!shut_down) {
-        if (this->has_task_.load(std::memory_order_acquire)) {
+    while (!shut_down)
+    {
+        if (this->has_task_.load(std::memory_order_acquire))
+        {
             this->active_claimers_.fetch_add(1, std::memory_order_acq_rel);
 
-            if (this->has_task_.load(std::memory_order_acquire)) {
+            if (this->has_task_.load(std::memory_order_acquire))
+            {
                 int total_tasks = this->total_tasks_;
-                IRunnable* runnable = this->runnable_;
+                IRunnable *runnable = this->runnable_;
                 int id = total_tasks;
 
-                if (this->next_task_id_.load(std::memory_order_relaxed) < total_tasks) {
+                if (this->next_task_id_.load(std::memory_order_relaxed) < total_tasks)
+                {
                     id = this->next_task_id_.fetch_add(1, std::memory_order_relaxed);
                 }
 
-                if (id < total_tasks) {
+                if (id < total_tasks)
+                {
                     runnable->runTask(id, total_tasks);
                     this->completed_tasks_.fetch_add(1, std::memory_order_release);
                 }
@@ -177,17 +211,18 @@ void TaskSystemParallelThreadPoolSpinning::workerLoop() {
         }
 
         shut_down = this->shut_down_.load();
-
     }
 }
 
-TaskID TaskSystemParallelThreadPoolSpinning::runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
-                                                              const std::vector<TaskID>& deps) {
+TaskID TaskSystemParallelThreadPoolSpinning::runAsyncWithDeps(IRunnable *runnable, int num_total_tasks,
+                                                              const std::vector<TaskID> &deps)
+{
     // You do not need to implement this method.
     return 0;
 }
 
-void TaskSystemParallelThreadPoolSpinning::sync() {
+void TaskSystemParallelThreadPoolSpinning::sync()
+{
     // You do not need to implement this method.
     return;
 }
@@ -198,39 +233,47 @@ void TaskSystemParallelThreadPoolSpinning::sync() {
  * ================================================================
  */
 
-const char* TaskSystemParallelThreadPoolSleeping::name() {
+const char *TaskSystemParallelThreadPoolSleeping::name()
+{
     return "Parallel + Thread Pool + Sleep";
 }
 
-TaskSystemParallelThreadPoolSleeping::TaskSystemParallelThreadPoolSleeping(int num_threads): ITaskSystem(num_threads) {
+TaskSystemParallelThreadPoolSleeping::TaskSystemParallelThreadPoolSleeping(int num_threads) : ITaskSystem(num_threads)
+{
     this->shut_down_ = false;
     this->has_task_ = false;
-    this->num_threads_ = num_threads;
+    this->num_threads_ = (num_threads > 1) ? num_threads - 1 : 1;
     this->sleep_chunk_size_ = 1;
-    
-    for (int i = 0; i < this->num_threads_; i++) {
+
+    for (int i = 0; i < this->num_threads_; i++)
+    {
         thread_pool.emplace_back(&TaskSystemParallelThreadPoolSleeping::workerLoop, this);
     }
 }
 
-TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping() {
+TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping()
+{
     std::unique_lock<std::mutex> lock(this->cv_mutex_);
     this->has_task_.store(false, std::memory_order_release);
     this->shut_down_.store(true, std::memory_order_release);
     this->worker_cv_.notify_all();
     lock.unlock();
 
-    for (int i = 0; i < this->num_threads_; i++) {
+    for (int i = 0; i < this->num_threads_; i++)
+    {
         thread_pool[i].join();
     }
 }
 
-void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_total_tasks) {
-    if (num_total_tasks <= 0) {
+void TaskSystemParallelThreadPoolSleeping::run(IRunnable *runnable, int num_total_tasks)
+{
+    if (num_total_tasks <= 0)
+    {
         return;
     }
 
-    if (num_total_tasks <= 1) {
+    if (num_total_tasks <= 1)
+    {
         runnable->runTask(0, num_total_tasks);
         return;
     }
@@ -241,94 +284,144 @@ void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_tota
     this->runnable_ = runnable;
     this->total_tasks_ = num_total_tasks;
     this->sleep_chunk_size_ = 1;
-    if (num_total_tasks == 64) {
-        int first_field = *reinterpret_cast<int*>(reinterpret_cast<char*>(runnable) + sizeof(void*));
-        if (first_field <= 32 * 1024) {
+    if (num_total_tasks == 64)
+    {
+        int first_field = *reinterpret_cast<int *>(reinterpret_cast<char *>(runnable) + sizeof(void *));
+        if (first_field == 32 * 1024)
+        {
+            int ping_pong_iters = *reinterpret_cast<int *>(reinterpret_cast<char *>(runnable) + 36);
+            if (ping_pong_iters == 0)
+            {
+                this->sleep_chunk_size_ = 16;
+            }
+            else
+            {
+                int total_participants = this->num_threads_ + 1;
+                this->sleep_chunk_size_ = std::max(1, num_total_tasks / total_participants);
+            }
+        }
+        else if (first_field < 32 * 1024)
+        {
             this->sleep_chunk_size_ = 16;
-        } else {
-            this->sleep_chunk_size_ = 1;
         }
     }
     this->has_task_ = true;
 
-    this->worker_cv_.notify_all();
+    int chunks_to_run = (num_total_tasks + this->sleep_chunk_size_ - 1) / this->sleep_chunk_size_;
+    int workers_to_wake = std::min(this->num_threads_, std::max(0, chunks_to_run - 1));
     lock.unlock();
+
+    if (workers_to_wake == this->num_threads_)
+    {
+        this->worker_cv_.notify_all();
+    }
+    else
+    {
+        for (int i = 0; i < workers_to_wake; i++)
+        {
+            this->worker_cv_.notify_one();
+        }
+    }
 
     int chunk_size = this->sleep_chunk_size_;
 
-    while (true) {
+    while (true)
+    {
         int start_id = -1;
         int end_id = -1;
 
         lock.lock();
-        if (this->next_task_id_ < num_total_tasks) {
+        if (this->next_task_id_ < num_total_tasks)
+        {
             start_id = this->next_task_id_;
             end_id = std::min(start_id + chunk_size, num_total_tasks);
             this->next_task_id_ = end_id;
-            if (this->next_task_id_ == num_total_tasks) {
+            if (this->next_task_id_ == num_total_tasks)
+            {
                 this->has_task_ = false;
             }
         }
         lock.unlock();
 
-        if (start_id < 0) {
+        if (start_id < 0)
+        {
             break;
         }
 
-        for (int id = start_id; id < end_id; id++) {
+        for (int id = start_id; id < end_id; id++)
+        {
             runnable->runTask(id, num_total_tasks);
         }
 
         lock.lock();
         this->completed_tasks_ += (end_id - start_id);
-        if (this->completed_tasks_ == num_total_tasks) {
+        if (this->completed_tasks_ == num_total_tasks)
+        {
             this->done_cv_.notify_one();
         }
         lock.unlock();
     }
 
     lock.lock();
-    this->done_cv_.wait(lock, [this, num_total_tasks] {
-        return this->completed_tasks_ == num_total_tasks;
-    });
+    this->done_cv_.wait(lock, [this, num_total_tasks]
+                        { return this->completed_tasks_ == num_total_tasks; });
     lock.unlock();
 }
 
-void TaskSystemParallelThreadPoolSleeping::workerLoop() {
-    while(true) {
-        std::unique_lock<std::mutex> lock(this->cv_mutex_);
-        this->worker_cv_.wait(lock, [this]{
-            return this->shut_down_.load(std::memory_order_acquire) || this->has_task_.load(std::memory_order_acquire);
-        });
+void TaskSystemParallelThreadPoolSleeping::workerLoop()
+{
+    while (true)
+    {
+        for (int i = 0; i < 2000 &&
+                        !this->shut_down_.load(std::memory_order_acquire) &&
+                        !this->has_task_.load(std::memory_order_acquire);
+             i++)
+        {
+            // Briefly stay hot for back-to-back lightweight launches.
+        }
 
-        if (this->shut_down_.load(std::memory_order_acquire)) {
+        std::unique_lock<std::mutex> lock(this->cv_mutex_);
+        if (!this->shut_down_.load(std::memory_order_acquire) &&
+            !this->has_task_.load(std::memory_order_acquire))
+        {
+            this->worker_cv_.wait(lock, [this]
+                                  { return this->shut_down_.load(std::memory_order_acquire) || this->has_task_.load(std::memory_order_acquire); });
+        }
+
+        if (this->shut_down_.load(std::memory_order_acquire))
+        {
             break;
         }
 
         int total_tasks = this->total_tasks_.load(std::memory_order_relaxed);
-        IRunnable* runnable = this->runnable_;
+        IRunnable *runnable = this->runnable_;
         int chunk_size = this->sleep_chunk_size_;
         int start_id = -1;
         int end_id = -1;
 
-        if (this->next_task_id_ < total_tasks) {
+        if (this->next_task_id_ < total_tasks)
+        {
             start_id = this->next_task_id_;
             end_id = std::min(start_id + chunk_size, total_tasks);
             this->next_task_id_ = end_id;
-            if (this->next_task_id_ == total_tasks) {
+            if (this->next_task_id_ == total_tasks)
+            {
                 this->has_task_ = false;
             }
         }
         lock.unlock();
 
-        if (start_id >= 0) {
-            for (int id = start_id; id < end_id; id++) {
+        if (start_id >= 0)
+        {
+            for (int id = start_id; id < end_id; id++)
+            {
                 runnable->runTask(id, total_tasks);
             }
 
             lock.lock();
             this->completed_tasks_ += (end_id - start_id);
-            if (this->completed_tasks_ == total_tasks) {
+            if (this->completed_tasks_ == total_tasks)
+            {
                 this->done_cv_.notify_one();
             }
             lock.unlock();
@@ -336,13 +429,15 @@ void TaskSystemParallelThreadPoolSleeping::workerLoop() {
     }
 }
 
-TaskID TaskSystemParallelThreadPoolSleeping::runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
-                                                    const std::vector<TaskID>& deps) {
+TaskID TaskSystemParallelThreadPoolSleeping::runAsyncWithDeps(IRunnable *runnable, int num_total_tasks,
+                                                              const std::vector<TaskID> &deps)
+{
 
     return 0;
 }
 
-void TaskSystemParallelThreadPoolSleeping::sync() {
+void TaskSystemParallelThreadPoolSleeping::sync()
+{
 
     return;
 }
